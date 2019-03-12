cac3417bbf0963bfb71fe51f7baf86216fd991bd
yogeshu <yogeshbhavsar1994@gmail.com>
2019-03-12 18:22:30 +0530

    the responsive navbar hamburger

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
-import UserLogin from './components/userlogin';
-import Home from './components/home';
-import UserRegistration from './components/userregistration';
+import 'bootstrap/dist/js/bootstrap';
+
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
 
 import Gun from 'gun/gun';
+import Home from './components/home';
+import UserLogin from './components/userlogin';
+import UserRegistration from './components/userregistration';
+import logo from './logo.svg';
+
 require('gun/sea');
 
 class App extends Component {
@@ -14,22 +17,25 @@ class App extends Component {
     return (
     	<Router>
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to={'/'} className="navbar-brand">DGstudio App </Link>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                  <Link to={'/newUser'} className="nav-link">Sign Up</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/login'} className="nav-link">Log In</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/'} className="nav-link">Home</Link>
-                </li>
-              </ul>
-            </div>
-          </nav> <br/>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to={'/'} className="navbar-brand">DGstudio App </Link>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavDropdown">
+           <ul class="navbar-nav">
+             <li class="nav-item active">
+           <Link to={'/newUser'} className="nav-link">Sign Up</Link>
+             </li>
+             <li class="nav-item">
+            <Link to={'/login'} className="nav-link">Log In</Link>
+             </li>
+             <li class="nav-item">
+            <Link to={'/'} className="nav-link">Home</Link>
+             </li>
+           </ul>
+         </div>
+       </nav> <br/>
           <h2>DGstudio App</h2> <br/>
           <Switch>
               <Route exact path='/newUser' component={ UserRegistration } />
